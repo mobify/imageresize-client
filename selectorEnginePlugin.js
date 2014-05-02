a6f0033eee49d5b27484951b8b43ddffe9234f5f
Noah Adams <noah.adams@gmail.com>
2014-05-02 11:32:02 -0700

    adds jquery/zepto plugin for image resizing

---
+++ selectorEnginePlugin.js
@@ -0,0 +1,21 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['selectorLibrary','resizeImages'], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('selectorLibrary'), require('resizeImages'));
+    } else {
+        // Browser globals (root is window)
+        root.ResizeImages = factory(root.$, root.ResizeImages);
+    }
+}(this, function ($, ResizeImages) {
+    function resizeImages(opts) {
+        var imgs = this.filter('img').add(this.find('img')).toArray();
+        return $(ResizeImages.resize.call(window, imgs, opts));
+    }
+    $.fn.resizeImages = resizeImages;
+    return resizeImages;
+}));
